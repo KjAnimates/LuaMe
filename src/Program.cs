8e5de5319c78882bc79dca14c5130e0316e232b8
KJ <kjhouser43@gmail.com>
2023-08-07 18:14:09 -0400

    Hello World

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LuaMe
+{
+    public class Program
+    {
+        Console.Write("Test");
+    }
+}
